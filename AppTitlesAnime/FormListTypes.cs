45f3a01b3433deb46d58e146cfdc89da648dfb23
lizszay <hromtsovaelizaveta@yandex.ru>
2025-04-08 12:24:07 +0500

    Добавляем формы для работы со справочником Типы аниме

---
+++ AppTitlesAnime/FormListTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace AppTitlesAnime
+{
+    public partial class FormListTypes : Form
+    {
+        public FormListTypes()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddType_Click(object sender, EventArgs e)
+        {
+            FormAddType formAddType = new FormAddType();
+            formAddType.ShowDialog();
+        }
+    }
+}
